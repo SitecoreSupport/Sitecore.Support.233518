ed86cc60742c13c31c0f1014a7617157f31f70fb
Eugene Oparyshev <eop@sitecore.net>
2018-08-06 15:23:22 +0300

    github patch

---
+++ src/Sitecore.Support.233518/EmailCampaign/Cm/Commands/TaskCommands/CleanConfirmationIDs.cs
@@ -0,0 +1,16 @@
+﻿namespace Sitecore.Support.EmailCampaign.Cm.Commands.TaskCommands
+{
+    using Sitecore.Data.Items;
+    using Sitecore.Tasks;
+    public class CleanConfirmationIDs
+    {
+        public void Execute(Item[] itemArray, CommandItem commandItem, ScheduleItem scheduledItem)
+        {
+            lock (typeof(CleanConfirmationIDs))
+            {
+                var manager = Sitecore.Configuration.Factory.CreateObject("exm/subscriptionManager", true) as Sitecore.Support.EmailCampaign.Cm.SubscriptionManager;
+                manager.RemoveObsoleteMappings();
+            }
+        }
+    }
+}
--- src/Sitecore.Support.233518/EmailCampaign/Cm/SubscriptionManager.cs
+++ src/Sitecore.Support.233518/EmailCampaign/Cm/SubscriptionManager.cs
@@ -1,17 +1,57 @@
 ﻿namespace Sitecore.Support.EmailCampaign.Cm
 {
+    using System;
+    using System.Text.RegularExpressions;
+    using Sitecore.Data;
     using Sitecore.EmailCampaign.Cm.Factories;
+    using Sitecore.Modules.EmailCampaign;
     using Sitecore.Modules.EmailCampaign.Core;
     using Sitecore.Modules.EmailCampaign.Core.Contacts;
     using Sitecore.Modules.EmailCampaign.Factories;
     using Sitecore.Modules.EmailCampaign.ListManager;
     using Sitecore.Modules.EmailCampaign.Services;
+    using Sitecore.Data.Items;
+    using Sitecore.Data.Properties;
+    using Sitecore.Tasks;
+    using Sitecore.EmailCampaign.Model.Web.Settings;
+    using System.Xml;
+    using Sitecore.Configuration;
 
     public class SubscriptionManager : Sitecore.EmailCampaign.Cm.SubscriptionManager
     {
         public SubscriptionManager(IContactService contactService, Sitecore.ExM.Framework.Diagnostics.ILogger logger, ListManagerWrapper listManagerWrapper, IExmCampaignService exmCampaignService, PipelineHelper pipelineHelper, ISendingManagerFactory sendingManagerFactory, IManagerRootService managerRootService, IRecipientManagerFactory recipientManagerFactory) : base(contactService, logger, listManagerWrapper, exmCampaignService, pipelineHelper, sendingManagerFactory, managerRootService, recipientManagerFactory)
         {
 
+        }
+
+      [UsedImplicitly]
+        public double ConfirmationPeriod { get; set; }
+
+        /// <summary>
+        /// Removes old nodes from the dbo.Properties table
+        /// </summary>
+        public virtual void RemoveObsoleteMappings()
+        {
+            Database contentDb = Util.GetContentDb();
+            XmlNode confirmationPeriod = Factory.GetConfigNode("cleanSubsciptionIDsConfirmationPeriod");
+            DateTime value = DateTime.UtcNow.AddDays((double)(-System.Convert.ToDouble(confirmationPeriod.Attributes.Item(0).Value)));
+            foreach (string current in contentDb.DataManager.GetPropertyKeys("EmailCampaign"))
+            {
+                string[] array = contentDb.Properties[current].Split(new char[]
+                {
+            '|'
+                });
+                if (array.Length >= 3)
+                {
+                    string s = array[1];
+                    DateTime dateTime;
+                    if (DateTime.TryParse(s, out dateTime) && dateTime.CompareTo(value) < 0)
+                    {
+                        var x = Regex.Replace(current, $"{contentDb.PropertyStore.Prefix}_", string.Empty, RegexOptions.IgnoreCase);
+                        contentDb.Properties.Remove(x);
+                    }
+                }
+            }
         }
     }
 }
