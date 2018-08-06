266e315f7abb00c62d67c5b93ece6371a48863ee
Eugene Oparyshev <eop@sitecore.net>
2018-08-06 13:24:49 +0300

    the default implementation

---
+++ src/Sitecore.Support.233518/EmailCampaign/Cm/SubscriptionManager.cs
@@ -0,0 +1,17 @@
+﻿namespace Sitecore.Support.EmailCampaign.Cm
+{
+    using Sitecore.EmailCampaign.Cm.Factories;
+    using Sitecore.Modules.EmailCampaign.Core;
+    using Sitecore.Modules.EmailCampaign.Core.Contacts;
+    using Sitecore.Modules.EmailCampaign.Factories;
+    using Sitecore.Modules.EmailCampaign.ListManager;
+    using Sitecore.Modules.EmailCampaign.Services;
+
+    public class SubscriptionManager : Sitecore.EmailCampaign.Cm.SubscriptionManager
+    {
+        public SubscriptionManager(IContactService contactService, Sitecore.ExM.Framework.Diagnostics.ILogger logger, ListManagerWrapper listManagerWrapper, IExmCampaignService exmCampaignService, PipelineHelper pipelineHelper, ISendingManagerFactory sendingManagerFactory, IManagerRootService managerRootService, IRecipientManagerFactory recipientManagerFactory) : base(contactService, logger, listManagerWrapper, exmCampaignService, pipelineHelper, sendingManagerFactory, managerRootService, recipientManagerFactory)
+        {
+
+        }
+    }
+}
---
+++ src/Sitecore.Support.233518/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.233518")]
+[assembly: AssemblyProduct("Sitecore.Support.233518")]
+[assembly: ComVisible(false)]
